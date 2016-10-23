fd8620598938cd659150a0376fa8e6d6f1961084
cilsat <cil.satriawan@gmail.com>
2016-10-23 18:46:22 +0700

    surface_scale has an effect

--- util_vtk.py
+++ util_vtk.py
@@ -3,15 +3,7 @@ from vtk.util.colors import tomato
 import visdat
 import numpy as np
 
-def draw_wells(surface_scale=0.5, depth_scale=50.0):
-    #config.parse()
-    #p = slurp.get_bores(soilmap=config.config['soil'])
-    # axis scaling
-    #p.x *= surface_scale
-    #p.y *= surface_scale
-    #p.z *= depth_scale
-    #p.r *= depth_scale
-
+def draw_wells(surface_scale=1.0, depth_scale=50.0):
     p = visdat.read()
 
     ren = vtk.vtkRenderer()
@@ -50,34 +42,20 @@ def draw_wells(surface_scale=0.5, depth_scale=50.0):
     cm = vtk.vtkPolyDataMapper()
     cm.SetInputConnection(c.GetOutputPort())
 
-    #top = p.groupby(p.index.get_level_values(0)).first()
-    #btm = p.groupby(p.index.get_level_values(0)).z.last().tolist()
-
-    """
-    for n in xrange(len(top)):
-        ca = vtk.vtkActor()
-        ca.SetMapper(cm)
-        ca.GetProperty().SetColor(0.1, 0.8, 0.2)
-        x, y, z0 = top.ix[n, :3].tolist()
-        z1 = btm[n]
-        h = z0-z1
-        ca.SetPosition(x, z0-h, y)
-        ca.SetScale(1.0, 2.0*h, 1.0)
-        ren.AddActor(ca)
-    """
-
     colormap = make_colormap()
     for n in xrange(len(p)):
         ca = vtk.vtkActor()
         ca.SetMapper(cm)
         x, y, z0, z1, t, _, _ = p[n]
+        x *= surface_scale
+        y *= surface_scale
         z0 *= depth_scale
         z1 *= depth_scale
-
-        ca.GetProperty().SetColor(colormap[t])
         r = z0 - z1
         ca.SetPosition(x, z0-0.5*r, y)
         ca.SetScale(1.0, r, 1.0)
+
+        ca.GetProperty().SetColor(colormap[t])
         ren.AddActor(ca)
 
     legend = vtk.vtkLegendBoxActor()
