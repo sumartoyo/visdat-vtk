dd2764e9663d1f3a1304e5a4a74f35c03b96c942
cilsat <cil.satriawan@gmail.com>
2016-10-23 17:00:04 +0700

    pretty colors

--- util_vtk.py
+++ util_vtk.py
@@ -1,16 +1,18 @@
 import vtk
 from vtk.util.colors import tomato
-import slurp
-import config
+import visdat
+import numpy as np
 
 def draw_wells(surface_scale=0.5, depth_scale=5.0):
-    config.parse()
-    p = slurp.get_bores(soilmap=config.config['soil'])
+    #config.parse()
+    #p = slurp.get_bores(soilmap=config.config['soil'])
     # axis scaling
     #p.x *= surface_scale
     #p.y *= surface_scale
     #p.z *= depth_scale
     #p.r *= depth_scale
+
+    p = visdat.read()
 
     ren = vtk.vtkRenderer()
     win = vtk.vtkRenderWindow()
@@ -24,9 +26,10 @@ def draw_wells(surface_scale=0.5, depth_scale=5.0):
     cm = vtk.vtkPolyDataMapper()
     cm.SetInputConnection(c.GetOutputPort())
 
-    top = p.groupby(p.index.get_level_values(0)).first()
-    btm = p.groupby(p.index.get_level_values(0)).z.last().tolist()
+    #top = p.groupby(p.index.get_level_values(0)).first()
+    #btm = p.groupby(p.index.get_level_values(0)).z.last().tolist()
 
+    """
     for n in xrange(len(top)):
         ca = vtk.vtkActor()
         ca.SetMapper(cm)
@@ -37,13 +40,19 @@ def draw_wells(surface_scale=0.5, depth_scale=5.0):
         ca.SetPosition(x, z0-h, y)
         ca.SetScale(1.0, 2.0*h, 1.0)
         ren.AddActor(ca)
+    """
 
+    colormap = {}
     for n in xrange(len(p)):
         ca = vtk.vtkActor()
         ca.SetMapper(cm)
-        ca.GetProperty().SetColor(tomato)
-        x, y, z, r = p.ix[n, :].tolist()
-        ca.SetPosition(x, z, y)
+        x, y, z0, z1, t, _, _ = p[n]
+        if t not in colormap.iterkeys():
+            colormap[t] = np.random.ranf((1, 3)).flatten().tolist()
+
+        ca.GetProperty().SetColor(colormap[t])
+        r = z0 - z1
+        ca.SetPosition(x, z0-0.5*r, y)
         ca.SetScale(1.0, r, 1.0)
         ren.AddActor(ca)
 
